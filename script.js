821ff9fe8e3f5ceface42ce4903ba447628b9d49
icrcode-senai <icarobotelhosocial@gmail.com>
2026-03-12 14:38:31 -0300

    add - ex01

--- script.js
+++ script.js
@@ -1,42 +1,18 @@
-// console.log("Hello World");
-
-// let nome = "João";
-// console.log(nome);
-
-// const pi = 3.14;
-// console.log(pi);
-
-// let n1 = 1;
-// let n2 = 2;
-
-// console.log(n1 + n2);
-
-// function saudacao() {
-//     console.log("Olá meu nome é " + nome);
-// };
-
-// function somar(x, y) {
-//     console.log(x + y);
-// };
-
-// saudacao();
-// somar(4, 5);
-
-function somarNumeros() {
+// 1 – Soma de números informados
+function exercicio1() {
     let soma = 0;
     let continuar = true;
+
     while (continuar) {
-        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar): "));
-        // soma = soma + numero;
-        if (isNaN(numero)) {
-            alert("Por favor, digite um número válido!");
-        } else {
-            soma += numero;
-            console.log(soma);
+        let numero = Number(prompt("Digite um número:"));
+        soma += numero;
+
+        let resposta = prompt("Deseja continuar? (s/n)");
+
+        if (resposta == "n") {
+            continuar = false;
         }
-        continuar = confirm("Deseja adicionar mais um número.");
     }
-    alert("A soma dos números é: " + soma);
-    console.log("A soma dos números é: " + soma);
+
+    console.log("Soma total:", soma);
 }
-somarNumeros();
