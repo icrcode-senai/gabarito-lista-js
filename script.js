2e7ddbee72b45e424a392d0ebdf1058bd877f921
icrcode-senai <icarobotelhosocial@gmail.com>
2026-03-12 14:39:11 -0300

    add - ex03

--- script.js
+++ script.js
@@ -25,3 +25,29 @@ function exercicio2() {
 
     console.log("Lançamento realizado!");
 }
+
+// 3 – Balanço financeiro anual
+function exercicio3() {
+    let ganhoAnual = 0;
+    let gastoAnual = 0;
+
+    for (let i = 1; i <= 12; i++) {
+        let ganho = Number(prompt("Ganho do mês " + i));
+        let gasto = Number(prompt("Gasto do mês " + i));
+
+        ganhoAnual += ganho;
+        gastoAnual += gasto;
+    }
+
+    let saldo = ganhoAnual - gastoAnual;
+
+    console.log("Ganho anual:", ganhoAnual);
+    console.log("Gasto anual:", gastoAnual);
+    console.log("Saldo anual:", saldo);
+
+    if (saldo > 0) {
+        console.log("Lucro");
+    } else {
+        console.log("Prejuízo");
+    }
+}
